b3f4175e0747188a94dafd99071f5c02db69a057
Mert5558 <149884808+Mert5558@users.noreply.github.com>
2025-01-09 13:58:22 +0100

    Add files via upload

--- CPP05/ex00/Bureaucrat.cpp
+++ CPP05/ex00/Bureaucrat.cpp
@@ -6,11 +6,63 @@
 /*   By: merdal <merdal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 14:18:38 by merdal            #+#    #+#             */
-/*   Updated: 2025/01/08 14:18:57 by merdal           ###   ########.fr       */
+/*   Updated: 2025/01/09 13:57:35 by merdal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-Bureaucrat()
+Bureaucrat::Bureaucrat() :name("NoName"),grade(1)
 {}
+
+Bureaucrat(std::string name, int grade)
+{
+	
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat const &copy)
+{
+	*this = copy;
+}
+
+Bureaucrat &Bureaucrat::operator=(Bureaucrat const &copy)
+{
+	this->grade = copy.grade;
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat()
+{}
+
+Bureaucrat::GradeTooHighException() const throw()
+{
+	return ("Grade is too high!");
+}
+
+Bureaucrat::GradeTooLowException() const throw()
+{
+	return ("Grade is to low!");
+}
+
+Bureaucrat::getName()
+{
+	return (this->name);
+}
+
+Bureaucrat::getGrade()
+{
+	retrun(this->grade);
+}
+
+Bureaucrat::incrementGrade()
+{
+	if (this->grade -1 < 1)
+		throw (GradeTooHigh());
+	this->grade--;
+}
+Bureaucrat::decrementGrade()
+{
+	if (this->grade + 1 < 150)
+		throw (GradeTooLow());
+	this->grade++;
+}
