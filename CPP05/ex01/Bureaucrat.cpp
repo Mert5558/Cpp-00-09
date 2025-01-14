7ff6d249bf6f345059dd215fe1aa086f25a0787b
Mert5558 <149884808+Mert5558@users.noreply.github.com>
2025-01-14 12:36:12 +0100

    Add files via upload

---
+++ CPP05/ex01/Bureaucrat.cpp
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: merdal <merdal@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 14:18:38 by merdal            #+#    #+#             */
+/*   Updated: 2025/01/09 14:44:27 by merdal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() :name("NoName"),grade(1)
+{}
+
+
+Bureaucrat::Bureaucrat(Bureaucrat const &copy)
+{
+	*this = copy;
+}
+
+Bureaucrat &Bureaucrat::operator=(Bureaucrat const &copy)
+{
+	this->grade = copy.grade;
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat()
+{}
+
+Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return ("Grade is too high!");
+}
+
+Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return ("Grade is to low!");
+}
+
+Bureaucrat::Bureaucrat(std::string name, int grade)
+{
+	this->name = name;
+	if (grade < 1)
+		throw (GradeTooHighException());;
+	if (grade > 150)
+		throw (GradeTooLowException());
+	this->grade = grade;
+}
+void	Bureaucrat::getName()
+{
+	return (this->name);
+}
+
+Bureaucrat::getGrade()
+{
+	retrun(this->grade);
+}
+
+Bureaucrat::incrementGrade()
+{
+	if (this->grade -1 < 1)
+		throw (GradeTooHigh());
+	this->grade--;
+}
+Bureaucrat::decrementGrade()
+{
+	if (this->grade + 1 < 150)
+		throw (GradeTooLow());
+	this->grade++;
+}
---
+++ CPP05/ex01/Bureaucrat.hpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: merdal <merdal@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 13:07:54 by merdal            #+#    #+#             */
+/*   Updated: 2025/01/09 14:36:57 by merdal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+class Bureaucrat
+{
+	private:
+		const	std::string name;
+		int		grade;
+
+	public:
+		Bureaucrat();
+		Bureaucrat(std::string name, int grade);
+		Bureaucrat(const Bureaucrat &copy);
+		Bureaucrat &operator=(const Bureaucrat &copy);
+		~Bureaucrat();
+		
+		class GradeTooHighException: public std::exception
+		{
+			public:
+				virtual char const *what(void) const throw();
+		};
+		class GradeTooLowException: public std::exception
+		{
+			public:
+				virtual char const *what(void) const throw();
+		};
+
+		void	getName();
+		void	getGrade();
+		void	incrementGrade();
+		void	decrementGrade();
+};
