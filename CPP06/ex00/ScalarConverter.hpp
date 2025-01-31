bb288a250cd6c0f1da13fc193dc6b397ab4705cc
Mert5558 <149884808+Mert5558@users.noreply.github.com>
2025-01-31 14:07:00 +0100

    Add files via upload

---
+++ CPP06/ex00/ScalarConverter.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: merdal <merdal@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/31 11:18:20 by merdal            #+#    #+#             */
+/*   Updated: 2025/01/31 13:28:53 by merdal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+class ScalarConverter
+{
+	private:
+		
+	public:
+		ScalarConverter();
+		ScalarConverter(const ScalarConverter &copy);
+		ScalarConverter &operator=(const ScalarConverter &copy);
+		~ScalarConverter();
+		
+};
